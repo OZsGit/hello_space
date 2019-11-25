6d75a924c0408334d68521502defd6d4507a6b30
OZsGit <ole.zhang@odysseygeo.com.au>
2019-11-26 10:19:59 +1100

    initial commit

---
+++ main.c
@@ -0,0 +1,33 @@
+/********************************************************************************
+*			Myriota Module Application				*
+*										*
+* Application name:	hello_space						*			
+* Filename:		main.c							*
+* Date:			26 November 2019					*
+* File Version:		0.1							*	
+*										*		
+* Author:		Ole Zhang						*
+* Company:		Odyssey Geophysics					*
+*										*
+*********************************************************************************
+*										*
+* [space left empty] 								*
+*										*
+*********************************************************************************
+*										*
+* Description:	[short description]						*
+*	[detailed description]							*
+*										*				
+*********************************************************************************
+*										*
+* Pin assignments:								*
+*	[none]									*
+*										*
+*										*
+********************************************************************************/
+#include "myriota_user_api.h"
+
+void AppInit() {
+   printf("Hello, Space!\n");
+}
+
