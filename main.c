e0ff21d844b1fb0a931dc1580b05678eb4667389
OZsGit <ole.zhang@odysseygeo.com.au>
2019-11-26 11:20:37 +1100

    step 2 in tutorial, prints Hello, Space! once every 5 seconds

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 * Application name:	hello_space						*			
 * Filename:		main.c							*
 * Date:			26 November 2019					*
-* File Version:		0.1							*	
+* File Version:		0.2							*	
 *										*		
 * Author:		Ole Zhang						*
 * Company:		Odyssey Geophysics					*
@@ -15,7 +15,7 @@
 *										*
 *********************************************************************************
 *										*
-* Description:	[short description]						*
+* Description:	Prints 'Hello, Space!' in the serial monitor once every 5 secs	*
 *	[detailed description]							*
 *										*				
 *********************************************************************************
@@ -27,7 +27,19 @@
 ********************************************************************************/
 #include "myriota_user_api.h"
 
-void AppInit() {
+time_t HelloSpace() { //the Job, called HelloSpace()
    printf("Hello, Space!\n");
+return SecondsFromNow(5); //time_t SecondsFromNow(unsigned Secs)
+}
+
+void AppInit() {
+   ScheduleJob(HelloSpace, ASAP()); 
+	/*time_t ScheduleJob (ScheduledJob Job, time_t Time)
+	ScheduleJob has two arguments: 
+		Job: a task to perform. It has the signature:
+			typedef time_t(* ScheduledJob )(void)
+				returns a value which is of type time_t, when the job should next run (after completing this one)
+		Time: an enum, type time_t. In this case ASAP(void) is as soon as possible	
+	*/
 }
 
